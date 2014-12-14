ab41d0e900a3b89a0469c914bafcb3d1d1f7b468
Mike Bright <github@mjbright.net>
2014-12-14 20:44:39 +0100

    Corrected main to return an int

--- language-helloworlds/HelloWorld.cpp
+++ language-helloworlds/HelloWorld.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 
 using namespace std;
-void main() {
-    cout << "Hello World from C++ !!" << endl;
-}
 
+int main() {
+    cout << "Hello World from C++ !!" << endl;
+    return 1;
+}
 
